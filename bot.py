9a4879f58b5f422493a87f2aafd9f280dc790eeb
Sarkar879 <v.ijaykumar76889888@gmail.com>
2025-01-31 16:19:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from threading import Thread
 
 loop = asyncio.get_event_loop()
 
-TOKEN = os.getenv("7375766856:AAEKEeVDdcsryYx3v2_hfoCEHw0M1fx3G-Y")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MONGO_URI = os.getenv("MONGODB_URI")
 
 FORWARD_CHANNEL_ID = -1001826672472
